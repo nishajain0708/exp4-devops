57afd3e8b01ec4facb973cd6c8dd802b62144caf
NISHA <jainnisha0708@gmail.com>
2025-09-27 15:46:13 +0530

    added

---
+++ src/main/java/com/example/nisha/App.java
@@ -0,0 +1,12 @@
+package com.example.nisha;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Sum is: " + (a + b));
+    }
+}
